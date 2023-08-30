6cc6cd68e0b1e50e2a70d7cd362fe65c550639a1
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-08-30 17:42:12 +0300

    task-5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - check if node is root
+ * @node: node to check
+ * Return: 1 if root. 0 otherwise
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	return (1);
+}
