e322a9e3d4448fce325de40bfef993acc30ab113
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-08-30 17:28:46 +0300

    task-4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - check if a node is leaf
+ * @node: node to check
+ * Return: 1 if node. 0 otherwise
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+	return (1);
+}
