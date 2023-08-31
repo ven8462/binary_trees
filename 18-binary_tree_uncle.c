0d42e32a74bf892c03612380807bd8bd9b14e3e2
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-08-31 18:01:25 +0300

    task-18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find sibling
@@ -13,4 +14,15 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (node->parent->right);
 	else
 		return (node->parent->left);
+}
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to node to find uncle
+ * Return: pointer to uncle. NULL otherwise
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
 }
